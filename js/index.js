e3d92a1753e06e4d213a2af4fe02b7846793f26f
skyeyy <748572726@qq.com>
2018-11-20 15:50:52 +0800

    第一次上传

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true, // 循环模式选项
+    aautoplay:true,
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })        
